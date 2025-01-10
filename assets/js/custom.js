8868f5e7c7404eb3583a96c2d8ecf95fdf9fbb36
amanpreet756 <amnkaursdimt2022@gmail.com>
2025-01-10 18:47:19 +0530

    Initial commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,50 +1,5 @@
 $(document).ready(function(){
-    $("#btn1").click(function(){
-      $("#btn2").toggle();
-    });
-
-   $("#btn2").click(function()
-{
-    $("p").toggle();
-});
-
-$(".slider").hover(function(){
-    $(".slider2").slideUp("slow");
-});
-
-
-    $("button").click(function(){
-      $(".box").animate({
-         left: '+=65px',
-        opacity: '0.5',
-        height: 'toggle',
-        width: '150px'
-      });
-    });
-   
-        $(".box_slide").click(function(){
-          $(".down_text").slideDown(1000);
-        });
-        $(".box_slide").hover(function(){
-          $(".down_text").stop();
-        });
-        $("#btn1").click(function(){
-          var total;
-          var basic_salary = parseInt($("#salary").val()); //document.getElementById("salary").value;
-          var option = $("[name='Gender']");
-        var hra = basic_salary * (20 / 100);
-          var da = basic_salary * (20 / 100);
-          var ta = basic_salary * (30 / 100);
-          var sa= basic_salary * (10/100);
-          total= hra + da + ta + basic_salary;
-          for(var a of option){
-            if(a.value=="Female"){
-              total += sa;
-            }
-          }
-          console.log('total',total);
-        }); 
-        $("#signup_sec_form").validate({
+           $("#signup_sec_form").validate({
           rules: {
          
             username: {
